72181aef0b08ede6c79de419b9c604a1aa905846
GloriaTM <gloria.fuchs@transfermarkt.de>
2018-04-05 15:40:40 +0200

    Update CBTBrowserTab.m

--- src/ios/CBTBrowserTab.m
+++ src/ios/CBTBrowserTab.m
@@ -50,9 +50,9 @@
   _safariViewController.delegate = self;
   
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.0")) {
-      _safariViewController.preferredBarTintColor = [self colorFromRGBA:  [themeableArgs objectForKey:@"toolbarColor"] ?: @"#1976D2"];
+      _safariViewController.preferredBarTintColor = [self colorFromRGBA:  [themeableArgs objectForKey:@"toolbarColor"] ?: @"#000000"];
   } else {
-      _safariViewController.view.tintColor = [self colorFromRGBA:  [themeableArgs objectForKey:@"toolbarColor"] ?: @"#1976D2"];
+      _safariViewController.view.tintColor = [self colorFromRGBA:  [themeableArgs objectForKey:@"toolbarColor"] ?: @"#000000"];
   }
 
   [self.viewController presentViewController:_safariViewController animated:YES completion:nil];
